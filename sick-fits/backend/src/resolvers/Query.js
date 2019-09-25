d1d9e5c9892e2eb8486cc5f45b85fef8df591faf
William Holcombe <sphinxelite@Williams-iMac.local>
2019-09-25 14:19:00 -0600

    ready for apollo

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,12 +1,19 @@
 const { forwardTo } = require("prisma-binding");
 
 const Query = {
+  //allows use same api on client and backend
   items: forwardTo("db")
-  // async items(parent, args, ctx, info) {
-  //   console.log('Getting Items!!');
-  //   const items = await ctx.db.query.items();
-  //   return items;
-  // },
+  //async items(parent, args, ctx, info) {
+  // console.log("getting items");
+  // const items = await ctx.db.query.items();
+  // return items;
 };
+
+// async items(parent, args, ctx, info) {
+//   console.log('Getting Items!!');
+//   const items = await ctx.db.query.items();
+//   return items;
+// },
+//};
 
 module.exports = Query;
